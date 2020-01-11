e05196b22f6140e6103455f29f6600f9a3ec9531
Pavan S Sabnis <pvnsbns@gmail.com>
2020-01-10 19:05:37 -0800

    Add player class and main run loop

--- client.py
+++ client.py
@@ -7,6 +7,35 @@ pygame.display.set_caption("Client") # Window caption
 
 clientNumber = 0  # Keeps track of number of connected clients
 
+# Player class represents rectangle that moves around the screen
+class Player():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.rect = (x,y,width,height)
+
+    def draw(self, window):
+        # Draw a rectangle that represents the player onto screen
+        pygame.draw.rect(window, self.color, self.rect)
+
+    
+
+
 def redrawWindow():
-    window.fill(255,255,255)
+    window.fill((255,255,255)) # Picks window color white
     pygame.display.update()
+
+
+def main():
+    run = True
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+        redrawWindow()
